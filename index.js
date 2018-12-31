baf3c6e43f0f3fc3344ab3bda78d978c59b4910e
Hristiyan Dodov <h.dodov@gmail.com>
2018-12-31 16:11:07 +0200

    Add Express template engine support

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-var path = require('path');
-var escape = require('escape-html');
 var config = require('./lib/config');
 var Renderer = require('./lib/renderer');
 
-exports.expressHandler = function (req, res) {
-    var file = path.join(config.webRoot, req.path);
-    var renderer = new Renderer(file, req);
+exports.__express = function (file, options, callback) {
+    var renderer = new Renderer(file, options);
 
-    return renderer.render().catch((error) => {
-        return Promise.resolve({
-            buffer: `<pre>${ escape(error.stack || error) }</pre>`
-        });
-    }).then((data) => {
-        res.send(data.buffer);
-    });
+    return renderer.render().then((data) => {
+        callback(null, data.buffer);
+    }).catch(callback);
 };
 
 exports.config = function (settings) {
     for (var k in settings) {
         config[k] = settings[k];
     }
-
-    return exports;
 };
